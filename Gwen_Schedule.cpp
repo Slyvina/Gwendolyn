33456e9e35ce1a70b040e203ad8ac519d5d45dde
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-12-03 23:18:41 +0100

    vUpdate

--- Gwen_Schedule.cpp
+++ Gwen_Schedule.cpp
@@ -22,7 +22,7 @@
 // 	Please note that some references to data like pictures or audio, do not automatically
 // 	fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 24.12.03 I
+// Version: 24.12.03 III
 // End License
 
 #include "Gwen_Schedule.hpp"
